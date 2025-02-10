84572fc5b80ce2277e80a910e6a2c5c5d33fb498
David Fryer <dfryer1193@gmail.com>
2025-02-10 14:09:32 -0500

    Dump request with headers

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http/httputil"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
 )
@@ -8,6 +10,17 @@ import (
 func main() {
 	router := gin.Default()
 	router.POST("/", func(ctx *gin.Context) {
+		log.Info().
+			Str("host", ctx.Request.Host).
+			Str("remoteAddr", ctx.Request.RemoteAddr).
+			Str("uri", ctx.Request.RequestURI)
+
+		requestDump, err := httputil.DumpRequest(ctx.Request, true)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Failed to dump request")
+		}
+		log.Info().Str("req", string(requestDump))
+
 		var body any
 		if err := ctx.ShouldBind(&body); err != nil {
 			log.Fatal().Err(err).Msg("Couldn't parse body")
